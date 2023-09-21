becbec1e683b4414bb795bc24c3adc739ee4cc27
Jose-Vela <joseeloyvo@gmail.com>
2023-09-21 05:01:55 -0600

    Initial Commit from IntelliJ IDEA

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,81 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner teclado = new Scanner(System.in);
+    static ArrayList<Vehiculo> vehiculos = new ArrayList<>();
+
+    public static void main(String[] args) {
+        String opcion;
+
+        do {
+            System.out.println("\n\n1 - Agregar vehiculo");
+            System.out.println("2 - Mostrar información del vehículo más barato");
+            System.out.println("3 - Mostrar todos los vehículos");
+            System.out.println("4 - Salir");
+            System.out.println("\nElija una de las opciones anteriores...");
+
+            opcion=teclado.nextLine();
+
+            switch (opcion){
+                case "1": agregarVehiculo(); break;
+                case "2": mostrarMasBarato(); break;
+                case "3": mostrarTodosLosVehiculos(); break;
+            }
+        } while (!opcion.equals("4"));
+        System.exit(0);
+    }
+
+    private static void mostrarTodosLosVehiculos() {
+        String marca, modelo;
+        float precio;
+
+        for(int i = 0; i< vehiculos.size(); i++) {
+            marca = vehiculos.get(i).getMarca();
+            modelo = vehiculos.get(i).getModelo();
+            precio = vehiculos.get(i).getPrecio();
+
+            System.out.println(i+1 + " - Marca: " + marca + ", Modelo: " + modelo + ", Precio:" + precio);
+        }
+        teclado.nextLine();
+    }
+
+    private static void mostrarMasBarato() {
+        int indiceMasBarato = calcularVehiculoMasBarato();
+        String marca = vehiculos.get(indiceMasBarato).getMarca();
+        String modelo = vehiculos.get(indiceMasBarato).getModelo();
+        float precio = vehiculos.get(indiceMasBarato).getPrecio();
+
+        System.out.println("Marca: " + marca + ", Modelo: " + modelo + ", Precio:" + precio);
+
+    }
+
+    private static int calcularVehiculoMasBarato() {
+        int indice = 0;
+        float precioMasBarato = vehiculos.get(0).getPrecio();
+        for(int i = 1; i < vehiculos.size(); i++) {
+            if (vehiculos.get(i).getPrecio() < precioMasBarato) {
+                indice = i;
+            }
+        }
+        return indice;
+    }
+
+    private static void agregarVehiculo() {
+        String marca, modelo;
+        float precio;
+
+        System.out.print("\nMarca: ");
+        marca = teclado.nextLine();
+        System.out.print("Modelo: ");
+        modelo = teclado.nextLine();
+        System.out.print("Precio: ");
+        precio = teclado.nextFloat();
+        teclado.nextLine();
+
+        vehiculos.add(new Vehiculo(marca, modelo, precio));
+    }
+}
---
+++ src/main/java/org/example/Vehiculo.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private float precio;
+
+    public Vehiculo(String marca, String modelo, float precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+}
